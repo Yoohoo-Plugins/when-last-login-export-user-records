b94aac97421e3dc1cbd877ca2ff04793caa41daa
Andrew Lima <andrew@arctek.co.za>
2022-12-07 17:18:53 +0200

    Upsell inline settings page.
    * Upsell inline settings page for User statistics.

--- when-last-login-export-user-records-settings.php
+++ when-last-login-export-user-records-settings.php
@@ -16,3 +16,14 @@
 	
 	<a href='<?php echo admin_url( 'admin.php?page=when-last-login-settings&tab=export-user-records' ) . '&export=user-records&type=json&user_nonce=' . $all_user_records_nonce; ?>' target='_BLANK' class='button button-primary'><?php esc_html_e( 'Export to JSON', 'when-last-login-export-user-records' ); ?></a></td>
 </tr>
+
+<?php if ( ! class_exists( 'WhenLastLoginStatistics' ) ) { ?>
+	<tr>
+		<?php 
+			$wll_svg = plugins_url( 'images/wll-star.svg', __FILE__ );
+			$wll_svg = '<img style="vertical-align: middle; display: inline-block;" src="' . esc_url( $wll_svg ) . '" alt="When Last Login User Statistics" width="20" height="20" />';
+		?>
+		<td colspan="2"><?php echo $wll_svg; ?> <span style="vertical-align: middle; display: inline-block;"><?php echo sprintf( 'If you need more advanced reporting, check out the %s', '<a href="https://yoohooplugins.com/plugins/when-last-login-user-statistics/?utm_source=export_records_plugin" target="_BLANK"><strong>' . esc_html__( 'When Last Login - User Statistics plugin' ) .  '</strong></a>' ); ?></span></td>
+	</tr>
+<?php } ?>
+
