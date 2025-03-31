d0a87672cb90a3a19bfc627abb9d7ec042ad7f12
Miguel Ermitao <m.ermitao@gmail.com>
2025-03-31 20:48:21 +0200

    Bug fix - Version 1.2

--- when-last-login-export-user-records.php
+++ when-last-login-export-user-records.php
@@ -4,7 +4,7 @@
  * Description: Exports all login records into a CSV file
  * Author: Yoohoo Plugins
  * Author URI: https://yoohooplugins.com
- * Version: 1.1
+ * Version: 1.2
  * License: GPL2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: when-last-login-export-user-records
@@ -12,6 +12,10 @@
  */
 
 /**
+ * 1.2
+ * Fixed "Export All User Records" to use WordPress timezone with get_date_from_gmt() instead of date_i18n().
+ * Updated fputcsv() to include escape parameter to address PHP deprecation warning.
+ * 
  * 1.0.1
  * Added in the ability to export user records
  * Separated export between login records and user records
@@ -22,175 +26,146 @@
  */
 class WhenLastLoginExportUserRecords {
 
-	public function __construct() {
+    public function __construct() {
+        add_filter( 'wll_settings_page_tabs', array( $this, 'wll_eur_settings_tab' ) );
+        add_filter( 'wll_settings_page_content', array( $this, 'wll_eur_settings_content' ) );
+        add_action( 'init', array( $this, 'wll_eur_export_records' ) );
+        add_action( 'admin_menu', array( $this, 'wll_eur_menu_item' ) );
+    }
 
-		add_filter( 'wll_settings_page_tabs', array( $this, 'wll_eur_settings_tab' ) );
-		add_filter( 'wll_settings_page_content', array( $this, 'wll_eur_settings_content' ) );
-		add_action( 'init', array( $this, 'wll_eur_export_records' ) );
-		add_action( 'admin_menu', array( $this, 'wll_eur_menu_item' ) );
+    public function wll_eur_menu_item() {
+        add_submenu_page( 'when-last-login-settings', __( 'Export Records', 'when-last-login-export-user-records' ), __( 'Export Records', 'when-last-login-export-user-records' ), 'manage_options', '?page=when-last-login-settings&tab=export-user-records' );
+    }
 
-	}
+    public function wll_eur_settings_tab( $array ) {
+        $array['export-user-records'] = array(
+            'title' => __( 'Export User Records', 'when-last-login-export-user-records' ),
+            'icon'  => '',
+        );
+        return $array;
+    }
 
-	public function wll_eur_menu_item() {
+    public function wll_eur_settings_content( $content ) {
+        $content['export-user-records'] = plugin_dir_path( __FILE__ ) . '/when-last-login-export-user-records-settings.php';
+        return $content;
+    }
 
-		add_submenu_page( 'when-last-login-settings', __( 'Export Records', 'when-last-login-export-user-records' ), __( 'Export Records', 'when-last-login-export-user-records' ), 'manage_options', '?page=when-last-login-settings&tab=export-user-records' );
+    /**
+     * Export All Login and All User Records.
+     * 
+     * @since 1.0
+     */
+    public function wll_eur_export_records() {
+        $export_array = array();
 
-	}
+        if ( isset( $_GET['tab'] ) && $_GET['tab'] == 'export-user-records' ) {
+            if ( isset( $_GET['export'] ) && isset( $_GET['type'] ) ) {
+                if ( $_GET['export'] == 'login-records' ) {
+                    if ( ! wp_verify_nonce( $_REQUEST['nonce'], 'wll_all_login_records_nonce' ) ) {
+                        return;
+                    }
 
-	public function wll_eur_settings_tab( $array ) {
+                    $args = array(
+                        'posts_per_page' => -1,
+                        'post_type'      => 'wll_records',
+                    );
 
-		$array['export-user-records'] = array(
-			'title' => __( 'Export User Records', 'when-last-login-export-user-records' ),
-			'icon'  => '',
-		);
+                    $the_query = new WP_Query( $args );
 
-		return $array;
+                    $export_array[] = array( 'title', 'author', 'email_address', 'date', 'ip_address' );
 
-	}
+                    if ( $the_query->have_posts() ) {
+                        while ( $the_query->have_posts() ) {
+                            $the_query->the_post();
+                            $ip_address = get_post_meta( get_the_ID(), 'wll_user_ip_address', true );
 
-	public function wll_eur_settings_content( $content ) {
+                            if ( $ip_address == '' ) {
+                                $ip_address = esc_html__( 'No IP Address Recorded', 'when-last-login-export-user-records' );
+                            }
 
-		$content['export-user-records'] = plugin_dir_path( __FILE__ ) . '/when-last-login-export-user-records-settings.php';
+                            $email_address = get_the_author_meta( 'user_email' );
 
-		return $content;
+                            $export_array[] = array(
+                                'title'         => sanitize_text_field( get_the_title() ),
+                                'author'        => sanitize_text_field( get_the_author() ),
+                                'email_address' => sanitize_email( $email_address ),
+                                'date'          => sanitize_text_field( get_the_date() ),
+                                'ip_address'    => sanitize_text_field( $ip_address ),
+                            );
 
-	}
+                            $export_array = apply_filters( 'wll_export_login_records_user_login_each', $export_array );
+                        }
+                        wp_reset_postdata();
+                    }
+                } elseif ( $_GET['export'] == 'user-records' ) {
+                    if ( ! wp_verify_nonce( $_REQUEST['user_nonce'], 'wll_all_user_records_nonce' ) ) {
+                        return;
+                    }
 
-	/**
-	 * Export All Login and All User Records.
-	 * 
-	 * @since 1.0
-	 */
-	public function wll_eur_export_records() {
+                    $users = get_users();
 
-		$export_array = array();
+                    $export_array[] = array( 'display_name', 'email_address', 'last_login', 'login_count' );
 
-		if ( isset( $_GET['tab'] ) && $_GET['tab'] == 'export-user-records' ) {
+                    foreach ( $users as $user ) {
+                        $last_logged_in  = get_user_meta( $user->ID, 'when_last_login', true );
+                        $logged_in_count = get_user_meta( $user->ID, 'when_last_login_count', true );
 
-			if ( isset( $_GET['export'] ) && isset( $_GET['type'] ) ) {
+                        if ( empty( $last_logged_in ) ) { // Use empty() to check for 0, '', null, false
+                            $formatted_logged_in = __( 'Never', 'when-last-login-export-user-records' );
+                        } else {
+                            // Ensure timestamp is numeric before formatting
+                            if ( is_numeric( $last_logged_in ) ) {
+                                $date_str_utc = gmdate( 'Y-m-d H:i:s', (int) $last_logged_in ); // Use gmdate for UTC
+                                $formatted_logged_in = get_date_from_gmt( $date_str_utc, 'Y-m-d H:i:s' );
+                            } else {
+                                $formatted_logged_in = __( 'Invalid Date', 'when-last-login-export-user-records' ); // Handle non-numeric meta
+                            }
+                        }
 
-				if ( $_GET['export'] == 'login-records' ) {
+                        $export_array[] = array(
+                            'display_name'  => sanitize_text_field( $user->data->display_name ),
+                            'email_address' => sanitize_email( $user->data->user_email ),
+                            'last_login'    => sanitize_text_field( $formatted_logged_in ),
+                            'login_count'   => sanitize_text_field( $logged_in_count ),
+                        );
 
-					
-					if ( ! wp_verify_nonce( $_REQUEST['nonce'], 'wll_all_login_records_nonce' ) ) {
-						return;
-					}
+                        $export_array = apply_filters( 'wll_export_user_records_user_login_each', $export_array );
+                    }
+                }
 
-					$args = array(
-						'posts_per_page' => -1,
-						'post_type'      => 'wll_records',
-					);
+                if ( $_GET['type'] == 'csv' ) {
+                    $fileName = time() . '-when-last-login-export-' . sanitize_text_field( $_GET['export'] ) . '.csv';
+                    header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+                    header( 'Content-Description: File Transfer' );
+                    header( 'Content-type: text/csv' );
+                    header( 'Content-Disposition: attachment; filename=' . sanitize_text_field( $fileName ) );
+                    header( 'Expires: 0' );
+                    header( 'Pragma: public' );
+                    $fh = @fopen( 'php://output', 'w' );
 
-					$the_query = new WP_Query( $args );
+                    foreach ( $export_array as $record ) {
+                        fputcsv( $fh, $record, ',', '"', '\\' );
+                    }
 
-					$export_array[] = array( 'title', 'author', 'email_address', 'date', 'ip_address' );
+                    fclose( $fh );
+                    exit();
+                } elseif ( $_GET['type'] == 'json' ) {
+                    $fileName = time() . '-when-last-login-export-' . sanitize_text_field( $_GET['export'] ) . '.json';
+                    header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+                    header( 'Content-Description: File Transfer' );
+                    header( 'Content-type: text/json' );
+                    header( 'Content-Disposition: attachment; filename=' . sanitize_text_field( $fileName ) );
+                    header( 'Expires: 0' );
+                    header( 'Pragma: public' );
+                    $fh = @fopen( 'php://output', 'w' );
 
-					if ( $the_query->have_posts() ) {
-
-						while ( $the_query->have_posts() ) {
-
-							$the_query->the_post();
-
-							$ip_address = get_post_meta( get_the_ID(), 'wll_user_ip_address', true );
-
-							if ( $ip_address == '' ) {
-								$ip_address = esc_html__( 'No IP Address Recorded', 'when-last-login-export-user-records' );
-							}
-
-							$email_address = get_the_author_meta( 'user_email' );
-
-							$export_array[] = array(
-								'title'         => sanitize_text_field( get_the_title() ),
-								'author'        => sanitize_text_field( get_the_author() ),
-								'email_address' => sanitize_email( $email_address ),
-								'date'          => sanitize_text_field( get_the_date() ),
-								'ip_address'    => sanitize_text_field( $ip_address ),
-							);
-
-							$export_array = apply_filters( 'wll_export_login_records_user_login_each', $export_array );
-
-						}
-
-						wp_reset_postdata();
-
-					}
-				} elseif ( $_GET['export'] == 'user-records' ) {
-
-					if ( ! wp_verify_nonce( $_REQUEST['user_nonce'], 'wll_all_user_records_nonce' ) ) {
-						return;
-					}
-
-					$users = get_users();
-
-					$export_array[] = array( 'display_name', 'email_address', 'last_login', 'login_count' );
-
-					foreach ( $users as $user ) {
-
-						$last_logged_in  = get_user_meta( $user->ID, 'when_last_login', true );
-						$logged_in_count = get_user_meta( $user->ID, 'when_last_login_count', true );
-
-						if ( $last_logged_in == 0 ) {
-							$formatted_logged_in = __( 'Never', 'when-last-login-export-user-records' );
-						} else {
-							$formatted_logged_in = date( 'Y-m-d H:i:s', (int) $last_logged_in );
-						}
-
-						$export_array[] = array(
-							'display_name'  => sanitize_text_field( $user->data->display_name ),
-							'email_address' => sanitize_email( $user->data->user_email ),
-							'last_login'    => sanitize_text_field( $formatted_logged_in ),
-							'login_count'   => sanitize_text_field( $logged_in_count ),
-						);
-
-						$export_array = apply_filters( 'wll_export_user_records_user_login_each', $export_array );
-
-					}
-				}
-
-				if ( $_GET['type'] == 'csv' ) {
-
-					$fileName = time() . '-when-last-login-export-' . sanitize_text_field( $_GET['export'] ) . '.csv';
-
-					header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
-					header( 'Content-Description: File Transfer' );
-					header( 'Content-type: text/csv' );
-					header( 'Content-Disposition: attachment; filename=' . sanitize_text_field( $fileName ) );
-					header( 'Expires: 0' );
-					header( 'Pragma: public' );
-					$fh = @fopen( 'php://output', 'w' );
-
-					foreach ( $export_array as $record ) {
-						fputcsv( $fh, $record, ',', '"' );
-					}
-
-					fclose( $fh );
-
-					exit();
-
-				} elseif ( $_GET['type'] == 'json' ) {
-
-					$fileName = time() . '-when-last-login-export-' . sanitize_text_field( $_GET['export'] ) . '.json';
-
-					header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
-					header( 'Content-Description: File Transfer' );
-					header( 'Content-type: text/json' );
-					header( 'Content-Disposition: attachment; filename=' . sanitize_text_field( $fileName ) );
-					header( 'Expires: 0' );
-					header( 'Pragma: public' );
-					$fh = @fopen( 'php://output', 'w' );
-
-					fputs( $fh, json_encode( $export_array ) );
-
-					fclose( $fh );
-
-					exit();
-
-				}
-			}
-		}
-
-	}
-
+                    fputs( $fh, json_encode( $export_array ) );
+                    fclose( $fh );
+                    exit();
+                }
+            }
+        }
+    }
 }
 
 new WhenLastLoginExportUserRecords();
